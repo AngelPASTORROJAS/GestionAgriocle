47897577cb0c159587c211849db05463889c0676
Angel Daniel PASTOR ROJAS <angeldaniel.pastorrojas@gmail.com>
2024-07-18 13:14:34 +0200

    Ajoutez des fichiers projet.

---
+++ GestionAgriocle/App/Database/BuilderDb.cs
@@ -0,0 +1,56 @@
+﻿namespace GestionAgriocle.App.Database
+{
+    internal class BuilderDb
+    {
+        private readonly Db _db;
+
+        private Db Db {  get => _db; }
+
+        public BuilderDb(Db database)
+        {
+            _db = database;
+        }
+
+        private void CreateTables()
+        {
+            string tableName;
+            List<string> proprieties;
+            Dictionary<string, List<string>> tables = new Dictionary<string, List<string>>();
+
+            tableName = "Unite";
+            proprieties = new List<string>()
+            {
+                "unite VARCHAR(50) PRIMARY KEY NOT NULL AUTO_INCREMENT"
+            };
+
+            tableName ="Date";
+            proprieties = new List<string>
+            {
+                "date DATE PRIMARY KEY",
+            };
+
+            foreach (KeyValuePair<string, List<string>> table in tables)
+            {
+                QueryCreateTable(table.Key, table.Value);
+            }
+
+        }
+
+        private void CreateMiddleTables()
+        {
+        }
+
+        private string QueryCreateTable(string tableName, List<string> proprieties)
+        {
+            string query = $"CREATE TABLE {tableName} (";
+            for (int i = 0; i < proprieties.Count()-1; i++)
+            {
+                query += proprieties[i] +"";
+            }
+            query += proprieties[proprieties.Count()-1]+");";
+            return query;
+        }
+
+        //private void DeleteTables() { }
+    }
+}
---
+++ GestionAgriocle/App/Database/Db.cs
@@ -0,0 +1,50 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace GestionAgriocle.App.Database
+{
+    internal class Db
+    {
+        #region "Attributs"
+        private readonly string _server = "127.0.0.1";
+        private readonly string _uid = "root";
+        private readonly string _password = "";
+        private readonly string _database = "challenge";
+        private MySqlConnection _connection;
+        #endregion
+
+        #region "Propriétés"
+        public MySqlConnection Connection { get => _connection; }
+        #endregion
+
+        #region "Constructeur"
+        public Db(string server, string uid, string password, string database)
+        {
+            InitConnection(server, uid, password, database);
+        }
+        #endregion
+
+        #region "Méthodes"
+        private void InitConnection(string server, string uid, string password, string database)
+        {
+            string connectionString = $"server={server};uid={uid};pwd={password};database={database}";
+            _connection = new MySqlConnection(connectionString);
+        }
+
+        public void Query(string query)
+        {
+            try
+            {
+                Connection.Open();
+                MySqlCommand cmd = new MySqlCommand(query, Connection);
+                int i = cmd.ExecuteNonQuery();
+                Connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.WriteLine(ex.Source + " : " + ex.Message);
+            }
+        }
+        #endregion
+    }
+}
