2fec877a7ab110784f981dbac16a8f52495d555c
Angel Daniel PASTOR ROJAS <angeldaniel.pastorrojas@gmail.com>
2024-07-19 13:34:27 +0200

    feat: update

--- GestionAgriocle/Program.cs
+++ GestionAgriocle/Program.cs
@@ -6,6 +6,6 @@ string uid = "root";
 string password = "";
 string database = "challenge";
 Db db = new Db(server,uid,password,database);
-BuilderDb builderDb = new BuilderDb(d);
+BuilderDb builderDb = new BuilderDb(db);
 builderDb.Build();
 //builderDb.DropTables();
