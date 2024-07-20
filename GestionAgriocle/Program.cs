1dbee258027644e7461aef58b58719e9611a62ec
Angel Daniel PASTOR ROJAS <angeldaniel.pastorrojas@gmail.com>
2024-07-20 17:53:14 +0200

    docs : ajout de todo pour des futures améliorations

--- GestionAgriocle/Program.cs
+++ GestionAgriocle/Program.cs
@@ -1,5 +1,7 @@
 ﻿using GestionAgriocle.IHM;
 
+// TODO : Make a code documentation
+
 // TODO : Use logs in excepetion :
 //      log them more appropriately (for example, with a logging tool such as Serilog or NLog) or
 //      return them to the caller for finer management.
@@ -11,5 +13,7 @@
 // TODO: Use interface IDispose in Service :
 //      When the service holds a reference to a UserRepository instance which it must release.
 //      This ensures that any resources used by the service are properly cleaned up when it is no longer needed.
+
+// TODO : Build Test and more ...
 
 new Menu();
